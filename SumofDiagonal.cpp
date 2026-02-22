6dfa32c0a5ead541e1bc1362954d942f54b25e36
X3RG3 <irfanayon88@gmail.com>
2026-02-22 07:38:13 +0600

    sum of diagonal elements v3

--- SumofDiagonal.cpp
+++ SumofDiagonal.cpp
@@ -11,7 +11,7 @@ int main(){
     int sum = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i==j || (i+j)==(2-1)){
+            if(i==j || (i+j)==(n-1)){
                 sum+=a[i][j];
             }
         }
