7407a6140b9f218eb9c764c539891e32a0d609ac
X3RG3 <irfanayon88@gmail.com>
2026-02-28 00:43:34 +0600

    Folder

---
+++ Arrays/BubbleSortDSC.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+    void bubbleSort(int num[],int size){
+        int i,j,temp;
+        for(i=size-1;i>=0;i--){
+            for(j=1;j<=i;j++){
+                if(num[j-1]<num[j]){
+                    temp = num[j-1];
+                    num[j-1] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+    }
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+
+    bubbleSort(arr,n);
+
+    cout<<"Sorted Array Descending : "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    
+}
