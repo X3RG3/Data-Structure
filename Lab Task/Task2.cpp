bba5dab12c3b69c04d3228169aea007f24d1c00c
X3RG3 <irfanayon88@gmail.com>
2026-02-28 09:28:18 +0600

    sort

--- Lab Task/Task2.cpp
+++ Lab Task/Task2.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
-
 void selectionSort(int A[],int n){
     int i,j,k,temp;
-    for(i=0;i<n-1;i++){
+    for(i = 0;i<n-1;i++){
         k = i;
-        for(j=i+1;j<n;j++){
+        for(j = i+1;j<n;j++){
             if(A[j]<A[k]){
                 k = j;
             }
@@ -15,15 +14,11 @@ void selectionSort(int A[],int n){
         A[k] = temp;
     }
 }
-
 int main(){
-    int arr[] ={33,66,22,99,77,11,91};
+    int arr[]= {22,66,33,88,11,44,55,99};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     selectionSort(arr,size);
-
-    cout<<"Sorted Array :  ";
-
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
